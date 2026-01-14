42c22243848318259213c3e909918aa011c0078c
alievaleila <meliyeva452@gmail.com>
2026-01-14 22:18:57 +0400

    feat: add StarPattern4 with nested loops for custom console output

---
+++ src/stars/StarPattern4.java
@@ -0,0 +1,37 @@
+package stars;
+
+public class StarPattern4 {
+    public static void main(String[] args) {
+
+        for (int k = 0; k < 3; k++) {
+            System.out.print("*");
+            for (int i = 0; i <4; i++) {
+                System.out.print(" ");
+            }
+            System.out.println("*");
+            for (int i = 0; i < 6; i++) {
+                System.out.print(" ");
+            }
+            System.out.println();
+            for (int i = 0; i < 6; i++) {
+                System.out.print("=");
+            }
+            for (int i = 0; i <6 ; i++) {
+                System.out.print(" ");
+            }
+            System.out.println();
+            for (int i = 0; i < 6; i++) {
+                System.out.print("=");
+            }
+            System.out.println();
+            for (int i = 0; i < 3; i++) {
+                for (int j = 0; j < 6; j++) {
+                    System.out.print(" ");
+                }
+                System.out.println();
+            }
+        }
+
+
+    }
+}
