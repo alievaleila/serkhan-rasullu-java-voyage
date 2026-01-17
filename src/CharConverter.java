8715b31b99d977cfece4da7ff2e15351e9a4fa47
alievaleila <meliyeva452@gmail.com>
2026-01-17 21:23:05 +0400

    feat: add character case conversion logic using ASCII offset

---
+++ src/CharConverter.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class CharConverter {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        char lowerCaseChar=sc.nextLine().toLowerCase().charAt(0);
+        char upperCaseChar=(char)(lowerCaseChar-32);
+        System.out.println(upperCaseChar);
+    }
+}
