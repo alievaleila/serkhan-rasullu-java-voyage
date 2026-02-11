256d3355895ce6fee2a2a8d08123511471c52be6
alievaleila <meliyeva452@gmail.com>
2026-02-11 21:02:35 +0400

    feat: implement 2D array input and output logic

---
+++ src/CastLab.java
@@ -0,0 +1,6 @@
+public class CastLab {
+    public static void main(String[] args) {
+        int a=(int)2323233443l;
+        System.out.println(a);
+    }
+}
---
+++ src/matrix_reloaded/ArrayLab.java
@@ -0,0 +1,24 @@
+package matrix_reloaded;
+
+import java.util.Scanner;
+
+public class ArrayLab {
+    public static void main(String[] args) {
+        int [][] arr=new int[2][3];
+
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j <arr[i].length; j++) {
+                System.out.println("arr["+i+"]["+j+"]=");
+                arr[i][j]=new Scanner(System.in).nextInt();
+            }
+        }
+
+        for (int i = 0; i < arr.length ; i++) {
+            for (int j = 0; j < arr[i].length; j++) {
+                System.out.println(arr[i][j]+" ");
+
+            }
+            System.out.println();
+        }
+    }
+}
